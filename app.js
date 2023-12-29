f40a1a53da6a621355b11305913b1e748d4e0898
doug delpha <doug@saooti.com>
2023-12-29 23:52:38 +0100

    added accordian

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 new Vue({
   el: "#app",
   data: {
-    title: "Hello World!",
-    link: "http://google.com",
-    finishedLink: '<a href="http://google.com">Google</a>',
+    accordions: [
+      {
+        title: "Synchronous vs. Asynchronous Code",
+        content:
+          "In synchronous programming, code is run one after another, meaning each task must finish before the next one starts. This can lead to problems, especially when dealing with operations that take a significant amount of time. Asynchronous programming overcomes this by allowing tasks to be performed concurrently, improving responsiveness and efficiency.",
+        isOpen: false,
+      },
+      {
+        title: "Asynchronous Patterns in JavaScript",
+        content:
+          "JavaScript has many ways to handle asynchronous operations. One of the simplest is the <code>setTimeout</code> function.",
+        isOpen: false,
+      },
+    ],
   },
   methods: {
-    sayHello: function () {
-      this.title = "Hello!";
-      return this.title;
-    },
-  },
-  computed: {
-    sayHello: function () {
-      this.title = "Hello!";
-      return this.title;
+    toggleAccordion(index) {
+      this.accordions[index].isOpen = !this.accordions[index].isOpen;
     },
   },
 });
