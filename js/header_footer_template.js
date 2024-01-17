f613476bb0c286abfaf120acc4f55b2ad23120f5
Hanji <kaitothedraggy@gmail.com>
2024-01-17 19:19:31 +0100

    update notes

--- js/header_footer_template.js
+++ js/header_footer_template.js
@@ -40,7 +40,7 @@ class Head extends HTMLElement {
     this.innerHTML = `
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="css/simple.css" />
+      <link rel="stylesheet" href="css/style.css" />
       <link rel="stylesheet" href="css/prism.css" />  
     `;
   }
